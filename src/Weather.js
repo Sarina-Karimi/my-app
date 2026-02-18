4fb1c811d3d02475bf145c5ef775c7988a63a128
Sarina-Karimi <sarinakariimi@gmail.com>
2026-02-18 23:07:06 +0330

    added apikey

--- src/Weather.js
+++ src/Weather.js
@@ -28,7 +28,7 @@ export default function Weather() {
 
   // Search function
   const searchCity = useCallback(() => {
-    const apiKey = process.env.REACT_APP_API_KEY;
+    const apiKey = "b2a5adcct04b33178913oc335f405433";
     const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
